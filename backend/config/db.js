7f2a4bf58539e494bd7e2ea2b54dc9cd38d39c46
Aditya-Pawar-1 <pawaraditya200410@gmail.com>
2025-07-14 20:14:09 +0530

    database config updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URL);XMLDocument
+    await mongoose.connect(process.env.MONGO_DB_URL);
   } catch (err) {
     console.error("Couldn't connect to database", err.message);
     process.exit(1);
